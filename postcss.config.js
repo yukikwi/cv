dd02079ec6ae72239495246cfabe59bb8f1dfb8a
Pachara Chantawong <pachara2202@gmail.com>
2021-07-07 19:14:38 +0700

    Revert purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-require('@fullhuman/postcss-purgecss')({
-  content: [ './public/**/*.html', './src/**/*.vue' ],
-  defaultExtractor: (content) => {
-    const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
-    return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!cursor-move).+-move$/, /^router-link(|-exact)-active$/ ],
-})
+}
