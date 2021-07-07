0f276059906e2930d0c90a207425e7409093fa0b
Pachara Chantawong <pachara2202@gmail.com>
2021-07-07 19:06:50 +0700

    Clean up

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-require('@fullhuman/postcss-purgecss')({
-  content: [ './public/**/*.html', './src/**/*.vue' ],
-  defaultExtractor: (content) => {
-    const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
-    return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
-  },
-  whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!cursor-move).+-move$/, /^router-link(|-exact)-active$/ ],
-})
