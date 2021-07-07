dbab78da293a8d48e9d833dd08f8087c8591978a
Pachara Chantawong <pachara2202@gmail.com>
2021-07-07 19:20:42 +0700

    Minify image on build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'? '/cv/' : '/'
+    publicPath: process.env.NODE_ENV === 'production'? '/cv/' : '/',
+    configureWebpack: {
+        plugins: [
+            new ImageminPlugin({
+                disable: process.env.NODE_ENV !== 'production',
+            })
+        ]
+    }
 }
