a371ee126c6d5a8949688bcaaafc942c548daa6f
Pachara Chantawong <pachara2202@gmail.com>
2021-07-08 15:13:09 +0700

    Fix flag emoji not display on windows

--- src/components/ProfileReference.vue
+++ src/components/ProfileReference.vue
@@ -2,9 +2,15 @@
     <div>
         <div class="mt-5 ml-5">
             <ul class="list-disc">
-                <li>Thai 🇹🇭</li>
-                <li>English 🇺🇸</li>
+                <li>Thai <img class="inline" src="@/assets/img/th.webp" /></li>
+                <li>English <img class="inline" src="@/assets/img/us.png" /></li>
             </ul>
         </div>
     </div>
 </template>
+
+<style scoped>
+li{
+    line-height: 2rem;
+}
+</style>
