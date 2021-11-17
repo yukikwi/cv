4ea2d1b198e418bc33c185380064b268f5e856d8
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-11-18 00:38:24 +0700

    create layout of resume

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import React, { ReactElement } from 'react'
+import './Home.css'
 
-interface Props {
-  
-}
-
-function index({}: Props): ReactElement {
+function index(): ReactElement {
   return (
-    <div>
-      
+    <div className="a4 grid grid-cols-3">
+      <div className="sec-l-bg col-span-1 px-5">
+        A
+      </div>
+      <div className="sec-r-bg col-span-2 px-5">
+        B
+      </div>
     </div>
   )
 }
